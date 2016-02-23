115b668e8c4d5d784804e42f20a19f214ff1b7af
James Trimble <james.trimble@yahoo.co.uk>
2016-02-23 11:20:40 +0000

    Added a script to summarise instances

---
+++ utils/summarise_instance.py
@@ -0,0 +1,29 @@
+"""Read an instance in .input (optionally with .ndds) format from standard
+input, and write a summary of vertex and arc counts to standard output.
+"""
+
+import sys
+
+def summarise(lines):
+    count_lines = [line.split() for line in lines if len(line.split())==2]
+    if len(count_lines) > 2:
+        print >>sys.stderr, "Error: two many lines with two tokens"
+        sys.exit(1)
+    elif len(count_lines) == 0:
+        print >>sys.stderr, "Error: not enough lines with two tokens"
+        sys.exit(1)
+    elif len(count_lines) == 2:
+        n_ndds, n_arcs_from_ndd = (int(x) for x in count_lines[1])
+    else:
+        n_ndds, n_arcs_from_ndd = 0, 0
+
+    n_pairs, n_arcs_between_pairs = (int(x) for x in count_lines[0])
+
+    print "n_pairs,n_arcs_between_pairs,n_ndds,n_arcs_from_ndd,n_vertices,n_arcs"
+    print "{},{},{},{},{},{}".format(n_pairs, n_arcs_between_pairs,
+            n_ndds, n_arcs_from_ndd,
+            n_pairs + n_ndds,
+            n_arcs_between_pairs + n_arcs_from_ndd)
+
+if __name__=="__main__":
+    summarise(sys.stdin.readlines())
